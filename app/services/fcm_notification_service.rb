f2ef863e9823a7b0a6a6624b092dfa552a2a4610
vitaly liber <zenamax@gmail.com>
2025-07-13 18:54:27 +0000

    Configure the FCM notification service

--- app/services/fcm_notification_service.rb
+++ app/services/fcm_notification_service.rb
@@ -2,16 +2,21 @@ require "fcm"
 
 class FcmNotificationService
   def initialize(service_account)
-    service_account = JSON.parse(service_account) if service_account.is_a?(String)
-    @fcm = FCM.new(service_account["private_key"], service_account)
+    parsed_service_account = JSON.parse(service_account) if service_account.is_a?(String)
+    @fcm = FCM.new(StringIO.new(service_account), parsed_service_account["project_id"])
   end
 
   def send_notification(data:, topic:, external_key:)
-    if topic
+    data = JSON.parse(data) if data.is_a?(String)
+
+    if topic.present?
       send_to_topic(topic, data)
     else
-      tokens = MobileDevice.where(external_key:).pluck(:device_token)
-      send_to_devices(tokens, data)
+      token = MobileDevice.where(external_key:).pluck(:device_token).first || "xxx"
+
+      return unless token.present?
+
+      send_to_device(data.merge(token:))
     end
   rescue StandardError => e
     Rails.logger.error "Error sending FCM notification: #{e.message}"
@@ -26,9 +31,9 @@ class FcmNotificationService
     { success: true, response: response }
   end
 
-  def send_to_devices(tokens, payload)
-    response = @fcm.send(tokens, payload)
-    Rails.logger.info "FCM notification sent to devices: #{response}"
+  def send_to_device(payload)
+    response = @fcm.send_v1(payload)
+    Rails.logger.info "FCM notification sent to device: #{response}"
     { success: true, response: response }
   end
 end
