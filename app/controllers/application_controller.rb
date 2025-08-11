e6fec20e05ad2b33e0ef5c7a7256ef5c9e27a10d
vitaly liber <zenamax@gmail.com>
2025-08-11 19:36:53 +0000

    Redirect users to the root path if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :restrict_access
   include Pagy::Backend
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def restrict_access
+    if mobile_access.blank?
+      # If the session token is present but no MobileAccess is found, clear the session token
+      session.delete(:token)
+      unless request.path == root_path
+        redirect_to root_path, alert: "Access token is invalid or expired. Please log in again."
+      end
+    end
+  end
 
   def mobile_access
     return @_mobile_access if defined?(@_mobile_access)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :restrict_access
+
   def create
     if MobileAccess.find_by(server_token: params[:server_token])
       session[:token] = params[:server_token]
