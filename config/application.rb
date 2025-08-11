76b456cce79737706e206706f866ffc509f9708a
vitaly liber <zenamax@gmail.com>
2025-08-11 19:35:34 +0000

    Keep the session token for 360 days

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Pusher
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.session_store :cookie_store, key: "_pusher_session", expire_after: 360.days
   end
 end
